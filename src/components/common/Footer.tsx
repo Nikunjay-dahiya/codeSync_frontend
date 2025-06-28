ae1ee2478a8ba003023352909a83f2f4d8f9d2f7
Nikunjay <chaudharynikunjay@gmail.com>
2025-06-28 22:21:25 +0530

    Initial commit

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build by{"NIKUNJAY"}
+                <a
+                    className="text-primary underline underline-offset-1"
+                >
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
